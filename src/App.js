148030d62a826952b2d2f4a74f64db0b14474375
ZiedMabrouk <84295527+Zied-Mabrouk@users.noreply.github.com>
2022-05-20 18:44:12 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import HomePage from "./components/modules/HomePage/HomePage";
 import NavBar from "./components/modules/NavBar/NavBar";
 
 import "./App.css";
-import Footer from "./components/modules/Footer/Footer";
 import About from "./components/modules/About/About";
 
 function App() {
