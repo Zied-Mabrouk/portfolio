46321a5b15a633a17e0dd9a91e85a95379d500f3
Zied-Mabrouk <zied.mabrouk@esprit.tn>
2024-05-05 16:08:48 +0200

    font

--- src/components/modules/About/About.jsx
+++ src/components/modules/About/About.jsx
@@ -1,18 +1,20 @@
-import "./About.scss";
 import React from "react";
 import Triangle from "../../cores/Triangle/Triangle";
+import "./About.scss";
 
 const About = () => {
-  
   return (
-    <div className="about" id="about" >
+    <div className="about" id="about">
       <Triangle />
       <div className="about-me">
         <div className="about-me-text">
           <h1>
             <b>A</b>bout me
           </h1>
-          <h2>I'm passionate about programming in general and I found my interest in the blockchain.</h2>
+          <h2>
+            I'm passionate about web dev in general and I found my interest in
+            the blockchain.
+          </h2>
         </div>
         <div className="about-me-container">
           <img src="../assets/img/me.jpg" alt="me" />
